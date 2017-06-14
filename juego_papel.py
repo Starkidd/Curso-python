1f52ec7fd5820a62dd6108195c49d1794bdc1cd5
starkidd <kiddstar47@gmail.com>
2017-06-14 17:45:14 -0600

    falta

---
+++ juego_papel.py
@@ -0,0 +1,47 @@
+
+
+DIC={
+	'piedra':{
+        'papel': False,
+	    'tijera':True,
+    },
+    'papel':{
+        'piedra':True,
+        'tijera':False
+	},
+    'tijera':{
+        'piedra':False,
+        'papel': True
+    
+    }
+}
+
+
+
+
+if __name__ == '__main__':
+	cont=0
+	vic_user=0
+	vic_maq=0
+
+
+
+	while cont < 1:
+		Decicion=input("elegir entre piedra, papel y tijera")
+
+		
+		maquina=random.choice(list(DIC.keys()))
+
+		if args.Decicion=='piedra':
+			print()
+		
+
+	count+=1
+
+	
+		
+
+		
+	
+
+
---
+++ tarea13.py
@@ -0,0 +1,25 @@
+
+lista=[]
+SegundaLista=[]
+
+def llenarlista():
+	
+	for x in range(4):
+		num=input("Introducir un numero #--->")
+		lista.insert(0,num)
+
+
+llenarlista()
+
+
+def SegLista():
+	for x in range(4):
+		print(lista[0])
+
+SegLista()
+
+	
+
+
+	
+
