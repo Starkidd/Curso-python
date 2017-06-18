4b9607e0f955c97132ddd65a700dc56e55579921
starkidd <kiddstar47@gmail.com>
2017-06-18 16:52:52 -0600

    juego completado

--- juego_papel.py
+++ juego_papel.py
@@ -1,47 +1,74 @@
-
+import random
 
 DIC={
-	'piedra':{
-        'papel': False,
-	    'tijera':True,
-    },
+    'piedra':{
+        'papel':'papel' ,'tijera':'tijera','piedra':'empate'},
     'papel':{
-        'piedra':True,
-        'tijera':False
-	},
+        'piedra':'piedra','tijera':'tijera','papel':'empate'},
     'tijera':{
-        'piedra':False,
-        'papel': True
-    
-    }
-}
-
-
-
+        'piedra':'piedra','papel':'papel','tijera':'empate'}}
 
 if __name__ == '__main__':
-	cont=0
-	vic_user=0
-	vic_maq=0
+    count = 0
+    vic_user = 0
+    vic_maq = 0
+    nombre = input("Ingrese tu nombre ==> ")
+
+while count < 5:
+    
+    if vic_maq == 3  or vic_user == 3 :
+        print()
+        print(nombre+":"+str(vic_user)+"-----"+"Maquina:"+str(vic_maq))
+        break
+    
+    else:
+        print()
+        print(nombre+":"+str(vic_user)+"-----"+"Maquina:"+str(vic_maq))
+        Decision = input(nombre+": Eliges entre piedra, papel o tijera ==>")
+        maquina = random.choice(list(DIC[Decision].keys()))
+        print('Usuario Maquina: '+ maquina)
+    
+        if Decision == 'papel':
+            if maquina == 'tijera':
+                print('la Maquina ha ganado esta ronda')
+                vic_maq=vic_maq+1
+                
+            elif maquina == 'piedra':
+                print('Has ganado esta ronda')
+                vic_user = vic_user+1
+            elif maquina == 'papel':
+                print('EMPATE')
+                count = count-1
+            
+            
+        elif Decision == 'piedra':
+            if maquina == 'tijera':
+                print('Has ganado esta ronda')
+                vic_user = vic_user+1
+            elif maquina == 'piedra':
+                print('Empate')
+                count = count-1
+            elif maquina == 'papel':
+                print('La maquina ha ganado esta ronda')
+                vic_maq=vic_maq+1
+            
+            
+        elif Decision == 'tijera':
+            if maquina == 'tijera':
+                print('EMPATE')
+                count = count-1
+            elif maquina == 'piedra':
+                print('La maquina ha ganado esta ronda')
+                vic_maq = vic_maq+1
+            elif maquina == 'papel':
+                print('Has ganado esta ronda')
+                vic_user = vic_user+1
+        count = count + 1
+        
 
 
 
-	while cont < 1:
-		Decicion=input("elegir entre piedra, papel y tijera")
-
-		
-		maquina=random.choice(list(DIC.keys()))
-
-		if args.Decicion=='piedra':
-			print()
-		
-
-	count+=1
-
-	
-		
-
-		
-	
-
-
+if vic_maq == 3:
+    print('LA MAQUINA HA GANADO :( ')
+elif vic_user == 3:
+    print('FELICIDADES HAS GANADO :)')
