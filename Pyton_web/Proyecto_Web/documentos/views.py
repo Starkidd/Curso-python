14df15cf1831ca8f04276669e6efb4b0155efde0
starkidd <kiddstar47@gmail.com>
2017-07-05 17:49:55 -0600

    tercer dia de web

--- Pyton_web/Proyecto_Web/documentos/views.py
+++ Pyton_web/Proyecto_Web/documentos/views.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.views.generic import View
+from documentos.models import Documento
 
 from django.shortcuts import render
 
 # Create your views here.
+
+#definir metodo get
+
+class Documentos(View):
+	def get(self,request,*args, **kwargs):
+		docs = Documento.objects.all()
+		#SELECT * FROM documentos_documentos; 
+		context = {
+		'docs':docs,
+		'encabezado':'Mis Documentos'
+		}
+		return render(
+			request,
+			'documentos.html',
+			context)
